335539a3f08634db29c6f5e8e6bc4e3e1caf59c3
huyuncong <huyuncongh@gmail.com>
2021-05-13 23:58:48 -0700

    edits

--- benches/nizk.rs
+++ benches/nizk.rs
@@ -13,7 +13,7 @@ use merlin::Transcript;
 use criterion::*;
 
 fn nizk_prove_benchmark(c: &mut Criterion) {
-  for &s in [10, 12, 16].iter() {
+  for &s in [12, 13, 14, 15, 16, 17, 18, 19, 20].iter() {
     let plot_config = PlotConfiguration::default().summary_scale(AxisScale::Logarithmic);
     let mut group = c.benchmark_group("NIZK_prove_benchmark");
     group.plot_config(plot_config);
